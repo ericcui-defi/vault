e1a58af9635781e531ecfe7291f5b6b228a740ce
Eric Cui <ericcui2005@gmail.com>
2026-05-25 15:20:51 +0200

    Vault MVP

--- programs/vault/src/state.rs
+++ programs/vault/src/state.rs
@@ -3,6 +3,7 @@ use anchor_lang::prelude::*;
 #[derive(InitSpace)]
 #[account]
 
+// Data stored in the `data` field of the Account structure
 pub struct VaultState{
     pub authority: Pubkey,
     pub bump: u8,
